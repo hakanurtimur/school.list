5fa618fa8ddb559fd292e9c16b387917d134238d
Hakan Urtimur <dythakanurtimur@gmail.com>
2023-01-24 14:52:55 +0300

    first commit

---
+++ schoollist.js
@@ -0,0 +1,60 @@
+class School {
+    constructor(name, level, numberOfStudents){
+      this._name = name;
+      this._level = level;
+      this._numberOfStudents = numberOfStudents;
+    }
+  get name(){
+    return this._name
+  }
+  get level(){
+    return this._level
+  }
+  
+  set numberOfStudents(newNumberOfStudents) {
+    if(typeof newNumberOfStudents === 'number'){
+      return this._numberOfStudents
+    }else{  
+    console.log('Invalid input: numberOfStudents must be set to a Number.')
+    }
+  
+  }
+  quickFacts() {
+    console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`)
+  }
+  static pickSubstituteTeacher(substituteTeachers) {
+    return substituteTeachers[Math.floor(substituteTeachers.length * Math.random())]
+  }
+  
+  }
+  
+  class PrimarySchool extends School {
+    constructor(name, numberOfStudents, pickupPolicy){
+      super(name, 'primary', numberOfStudents);
+    this._pickupPolicy = pickupPolicy;
+  
+    }
+    get pickUpPolicy(){
+      return this._pickupPolicy;
+    }
+  }
+  
+  class HighSchool extends School{
+    constructor(name, numberOfStudents, x){
+      super(name, 'highschool', numberOfStudents);
+      this._x = x;
+    }
+    get x(){
+      return this._x;
+    }
+  
+  }
+  
+  const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.')
+  
+  lorraineHansbury.quickFacts();
+  console.log(School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']))
+  
+  const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field'])
+  console.log(alSmith.x)
+  
